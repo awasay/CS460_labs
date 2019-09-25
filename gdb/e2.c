6b2c6c81cbc25951ec38c47645e90c9d90fdd544
Abdul Wasay <awasay@g.harvard.edu>
2019-09-25 14:13:04 +0000

    push to server

--- gdb/e2.c
+++ gdb/e2.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int main(int argc, char **argv)
+{
+   char *buf;
 
-int main(int argc, char** argv){
-  int foo[1000];
-  for (int i =0; i<= 1050; i++){
-    foo[i] = i;
-  }  
-  return 0;
+   buf = malloc(1<<31);
 
+   fgets(buf, 1024, stdin);
+   printf("%s\n", buf);
+
+   return 1;
 }
