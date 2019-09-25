4265cc4f4dcaa1fe33166448a8e4cad48e3e4f81
Abdul Wasay <awasay@g.harvard.edu>
2019-09-25 12:26:15 +0000

    push to server

--- valgrind/e3.c
+++ valgrind/e3.c
@@ -7,14 +7,11 @@ int main(int argc, char** argv){
   int i;
   int *a;
 
-  for (i=0; i < 10; i++){
-    a = malloc(sizeof(int) * 100);
+  for (i=0; i < 1; i++){
+    a = malloc(sizeof(int)*100);
   }
 
-  for (i=0; i < 10; i++){
-    free(a);
-    a++;
-  }
+  free(a);
 
   return 0;
 }
